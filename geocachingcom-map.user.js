ca94b9f18497c32eed7f63f4615304629ba8229c
Moritz Ulrich <thekenny123@gmail.com>
2009-05-26 20:40:32 +0200

    Added a hiding of owned caches.

--- geocachingcom-map.user.js
+++ geocachingcom-map.user.js
@@ -4,27 +4,25 @@
 // @include        http://www.geocaching.com/map/*
 // ==/UserScript==
 
-
+//<input id="chkHideFound" type="checkbox" onclick="hideFound(this, true)"/>
 document.getElementById('chkHideFound').setAttribute('onclick', 'hideFoundModified(this, true)');
 
 //unsafeWindow provides access to the vars and options from the site scope
+
+
 unsafeWindow.hideFoundModified = function(e, b) {
-  mrks = unsafeWindow.mrks;
-  lgnds = unsafeWindow.lgnds;
-
-
-    var a, d = mrks.length;
+    var a, d = unsafeWindow.mrks.length;
     if (e.checked === true) {
         for (a = 0; a < d; a++) {
-            if (mrks[a].found === true) {
-                mrks[a].hide()
+            if (unsafeWindow.mrks[a].found === true) {
+                unsafeWindow.mrks[a].hide()
             }
         }
     } else {
         for (a = 0; a < d; a++) {
-            if (mrks[a].found === true) {
-                if (lgnds.byTypeID(mrks[a].wptTypeId).visible === true) {
-                    mrks[a].show()
+            if (unsafeWindow.mrks[a].found === true) {
+                if (unsafeWindow.lgnds.byTypeID(unsafeWindow.mrks[a].wptTypeId).visible === true) {
+                    unsafeWindow.mrks[a].show()
                 }
             }
         }
@@ -34,3 +32,27 @@ unsafeWindow.hideFoundModified = function(e, b) {
     }
 }
 
+//<input id="chkHideOwned" type="checkbox" onclick="hideOwned(this,true);"/>
+document.getElementById('chkHideOwned').setAttribute('onclick', 'hideOwnedModified(this, true)');
+
+unsafeWindow.hideOwnedModified = function(e, b) {
+    var a, d = unsafeWindow.mrks.length;
+    if (e.checked === true) {
+        for (a = 0; a < d; a++) {
+            if (unsafeWindow.mrks[a].owned === true) {
+                unsafeWindow.mrks[a].hide()
+            }
+        }
+    } else {
+        for (a = 0; a < d; a++) {
+            if (unsafeWindow.mrks[a].owned === true) {
+                if (unsafeWindow.lgnds.byTypeID(unsafeWindow.mrks[a].wptTypeId).visible === true) {
+                    unsafeWindow.mrks[a].show()
+                }
+            }
+        }
+    }
+    if (b) {
+        unsafeWindow.updateSideBarList()
+    }
+}
